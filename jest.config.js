bddfd76994d73a234f7fc7d534bee41c3c3cfb18
Mouaz Naji <150366708+Mouaz7@users.noreply.github.com>
2026-02-24 15:01:09 +0100

    test: Add jest moduleNameMapper for absolute backend imports to fix CI relative path resolution

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,13 @@ module.exports = {
   testMatch: [
     '**/tests/**/*.test.js',
   ],
+  moduleNameMapper: {
+    '^../../config/db/database$': '<rootDir>/config/db/database',
+    '^../../../config/db/database$': '<rootDir>/config/db/database',
+    '^../../src/services/(.*)$': '<rootDir>/src/services/$1',
+    '^../../../src/services/(.*)$': '<rootDir>/src/services/$1',
+    '^../../../routes/(.*)$': '<rootDir>/routes/$1'
+  },
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   verbose: true,
   testTimeout: 10000,
